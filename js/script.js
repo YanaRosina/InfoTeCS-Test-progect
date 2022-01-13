9d5e4a048815f7f4d8347a34ca93595fa919567e
Yana <rosina-yana98@mail.ru>
2022-01-14 00:18:37 +0300

    Added the function "getSort" of sorting table columns.

--- js/script.js
+++ js/script.js
@@ -1,3 +1,5 @@
+'use strict';
+
 let dataBaseUrl = 'db.json';
 const getData = async (dataBaseUrl) => {
     const res = await fetch(dataBaseUrl);
@@ -8,13 +10,7 @@ const getData = async (dataBaseUrl) => {
 
     return await res.json();
 };
-//Cut row
-/*function cutAboutText(str) {
-    if (str.length > 80){
-        return str.substr(0,80)+'...'
-    } else return str;
-}*/
-// Create table
+
 
 class Person {
     constructor(firstName, lastName, about, eyeColor) {
@@ -26,7 +22,7 @@ class Person {
 
     newHtmlTableRow() {
 
-    const tableBody = document.querySelector('.table_body'),
+        const tableBody = document.querySelector('.table_body'),
             tableRow = document.createElement('tr'),
             cellFirstName = document.createElement('td'),
             cellLastName = document.createElement('td'),
@@ -35,7 +31,7 @@ class Person {
 
 
         cellFirstName.innerHTML = this.firstName;
-        cellAbout.classList='module';
+        cellAbout.classList = 'module';
         cellLastName.innerHTML = this.lastName;
         cellAbout.innerHTML = this.about;
         cellEyeColor.innerHTML = this.eyeColor;
@@ -49,17 +45,33 @@ class Person {
     }
 
 }
+
 getData(dataBaseUrl)
     .then(data => {
         data.forEach((obj) => {
             new Person(obj.name.firstName, obj.name.lastName, obj.about, obj.eyeColor).newHtmlTableRow();
-            //console.log(obj.name.firstName, obj.name.lastName, obj.about, obj.eyeColor);     
         });
-
-        //console.log(Object.keys(data).length);
     });
 
+// Table Sort
+
+    const getSort = ({ target }) => {
+        const order = (target.dataset.order = -(target.dataset.order || -1));
+        const index = [...target.parentNode.cells].indexOf(target);
+        const collator = new Intl.Collator(['en', 'ru']);
+        const comparator = (index, order) => (a, b) => order * collator.compare(
+            a.children[index].innerHTML,
+            b.children[index].innerHTML
+        );
+
+        for (const tBody of target.closest('table').tBodies)
+            tBody.append(...[...tBody.rows].sort(comparator(index, order)));
+
+        for (const cell of target.parentNode.cells)
+            cell.classList.toggle('sorted', cell === target);
+    };
+
+    document.querySelectorAll('.table_sort thead').forEach(tableTH => tableTH.addEventListener('click', (event) => getSort(event)));
 
 
-
-//export default persons;
+ 
