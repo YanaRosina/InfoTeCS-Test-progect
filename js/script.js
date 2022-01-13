fdf385714b0c3ba78cfafd9f99daab0c93e97098
Yana <rosina-yana98@mail.ru>
2022-01-13 21:28:56 +0300

    Data output to the table has been fixed, the display of the "about" block in the table has been added

--- js/script.js
+++ js/script.js
@@ -1,4 +1,3 @@
-
 let dataBaseUrl = 'db.json';
 const getData = async (dataBaseUrl) => {
     const res = await fetch(dataBaseUrl);
@@ -9,50 +8,58 @@ const getData = async (dataBaseUrl) => {
 
     return await res.json();
 };
+//Cut row
+/*function cutAboutText(str) {
+    if (str.length > 80){
+        return str.substr(0,80)+'...'
+    } else return str;
+}*/
 // Create table
+
+class Person {
+    constructor(firstName, lastName, about, eyeColor) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.about = about;
+        this.eyeColor = eyeColor;
+    }
+
+    newHtmlTableRow() {
+
     const tableBody = document.querySelector('.table_body'),
-        tableRow = document.createElement('tr'),
-        cellFirstName = document.createElement('td'),
-        cellLastName = document.createElement('td'),
-        cellAbout = document.createElement('td'),
-        cellEyeColor = document.createElement('td');
+            tableRow = document.createElement('tr'),
+            cellFirstName = document.createElement('td'),
+            cellLastName = document.createElement('td'),
+            cellAbout = document.createElement('td'),
+            cellEyeColor = document.createElement('td');
 
-    class Person {
-        constructor(firstName, lastName, about, eyeColor) {
-            this.firstName = firstName;
-            this.lastName = lastName;
-            this.about = about;
-            this.eyeColor = eyeColor;
-        }
 
-        newHtmlTableRow() {
-            cellFirstName.innerHTML = this.firstName;
-            cellLastName.innerHTML = this.lastName;
-            cellAbout.innerHTML = this.about;
-            cellEyeColor.innerHTML = this.eyeColor;
+        cellFirstName.innerHTML = this.firstName;
+        cellAbout.classList='module';
+        cellLastName.innerHTML = this.lastName;
+        cellAbout.innerHTML = this.about;
+        cellEyeColor.innerHTML = this.eyeColor;
 
-            tableRow.appendChild(cellFirstName);
-            tableRow.appendChild(cellLastName);
-            tableRow.appendChild(cellAbout);
-            tableRow.appendChild(cellEyeColor);
-            tableBody.appendChild(tableRow);
-
-        }
+        tableRow.appendChild(cellFirstName);
+        tableRow.appendChild(cellLastName);
+        tableRow.appendChild(cellAbout);
+        tableRow.appendChild(cellEyeColor);
+        tableBody.appendChild(tableRow);
 
     }
-    getData(dataBaseUrl)
-    .then(data => {   
-        data.forEach((obj) => {    
+
+}
+getData(dataBaseUrl)
+    .then(data => {
+        data.forEach((obj) => {
             new Person(obj.name.firstName, obj.name.lastName, obj.about, obj.eyeColor).newHtmlTableRow();
             //console.log(obj.name.firstName, obj.name.lastName, obj.about, obj.eyeColor);     
-       });
-    
+        });
+
         //console.log(Object.keys(data).length);
     });
-    
-        
+
+
 
 
 //export default persons;
-
-
